8c12d364d851452fafdd9de720c78177451a30bb
Angeles <angie.oliva@gmail.com>
2021-11-02 22:12:53 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Este es el primer desafío de React en CoderHouse
         </p>
         <a
           className="App-link"
